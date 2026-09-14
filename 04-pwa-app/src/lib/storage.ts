962fb3b5a508793ca4fadebfeba8bc44fd0fa06f
kaka74mk-jpg <kaka74mk@gmail.com>
2026-09-15 01:44:35 +0330

    Delete 04-pwa-app directory

--- 04-pwa-app/src/lib/storage.ts
+++
@@ -1,7 +0,0 @@
-import { supabase } from "./supabaseClient";
-
-export async function getSignedUrl(bucket: string, path: string, expiresIn = 900) {
-  const { data, error } = await supabase.storage.from(bucket).createSignedUrl(path, expiresIn);
-  if (error) throw error;
-  return data.signedUrl;
-}
